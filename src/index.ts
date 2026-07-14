542880209ebef12ea9bfba7671a47e9718a5fc0b
ArashTvn <arashtrader9977@gmail.com>
2026-07-15 02:15:02 +0330

    feat: add guard public types

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export type {
+  ConfirmNavigationParams,
+  UnsavedChangesGuardTrigger,
+  UseUnsavedChangesGuardOptions,
+  UseUnsavedChangesGuardReturn
+} from './types';
---
+++ src/types.ts
@@ -0,0 +1,23 @@
+import type {MaybeRefOrGetter, Ref} from 'vue';
+import type {RouteLocationNormalized} from 'vue-router';
+
+export type UnsavedChangesGuardTrigger = 'route-leave' | 'route-update';
+
+export interface ConfirmNavigationParams {
+  trigger: UnsavedChangesGuardTrigger;
+  to: RouteLocationNormalized;
+  from: RouteLocationNormalized;
+}
+
+export interface UseUnsavedChangesGuardOptions {
+  hasUnsavedChanges: MaybeRefOrGetter<boolean>;
+  confirmNavigation: (params: ConfirmNavigationParams) => boolean | Promise<boolean>;
+  enabled?: MaybeRefOrGetter<boolean>;
+  guardRouteUpdates?: boolean;
+  guardBeforeUnload?: boolean;
+}
+
+export interface UseUnsavedChangesGuardReturn {
+  isConfirming: Ref<boolean>;
+  bypassNextNavigation: () => void;
+}
